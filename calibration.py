66c9d3a5881ca2e603cbfe0d0667fa26df0c3e5a
Caleb Hicks <crh14b@acu.edu>
2017-07-15 14:55:34 -0500

    changed plotter to look at last entry instead of average; often many 0 readings were included at the start

--- calibration.py
+++ calibration.py
@@ -26,27 +26,42 @@ for line in cf:
 	datarray = [[] for x in range(3)]
 	for event in f.tree:
 		if event.time > timestamp and event.time < timestamp + dt:
-			#print(str(event.time)+'\t'+str(event.channels[2])+'\t'+str(dt))
 			datarray[0].append(event.channels[1])
 			datarray[1].append(event.channels[3])
 			datarray[2].append(event.channels[5])	
-	currentLoop.append(sum(datarray[0])/len(datarray[0]))
-	countDeriv.append(sum(datarray[1])/len(datarray[1]))
-	transducer.append(sum(datarray[2])/len(datarray[2]))
+	#currentLoop.append(sum(datarray[0])/len(datarray[0]))
+	#countDeriv.append(sum(datarray[1])/len(datarray[1]))
+	#transducer.append(sum(datarray[2])/len(datarray[2]))
+	currentLoop.append(datarray[0][-1])
+	countDeriv.append(datarray[1][-1])
+	transducer.append(datarray[2][-1])
 	sotera.append(float(a[4]))
 	f.Close()
-#m = [np.polyfit(x,calculated,1) for x in [transducer,currentLoop,countDeriv,sotera]]
-#print('Linear Fits for measurements:')
-#print('Measurement\t| m\t| b')
-#names=['Transducer','Current Loop','Counter','Sotera']
-#for cn,n in enumerate(names): print(n+'\t| '+str(m[cn][0]) +'\t| '+ str(m[cn,1]))
+m = [np.polyfit(x,calculated,1) for x in [transducer,currentLoop,countDeriv,sotera]]
+print('Linear Fits for measurements:')
+print('Measurement\t| m\t\t| b')
+names=['Transducer','Current Loop','Counter Deriv','Sotera Dispaly']
+for cn,n in enumerate(names): print(n+'\t| '+str(m[cn][0]) +'\t| '+ str(m[cn][1]))
 plt.figure(1)
 plt.subplot(221)
 plt.plot(transducer,calculated,'.')
+a = np.array(transducer)
+b = m[0][0]*a+m[0][1]
+plt.plot(a,b,color='r')
 plt.subplot(222)
 plt.plot(currentLoop,calculated,'.')
+a = np.array(currentLoop)
+b = m[1][0]*a+m[1][1]
+plt.plot(a,b,color='r')
 plt.subplot(223)
 plt.plot(countDeriv,calculated,'.')
+a = np.array(countDeriv)
+b = m[2][0]*a+m[2][1]
+plt.plot(a,b,color='r')
 plt.subplot(224)
 plt.plot(sotera,calculated,'.')
+a = np.array(sotera)
+b = m[3][0]*a+m[3][1]
+plt.plot(a,b,color='r')
+
 plt.show()
